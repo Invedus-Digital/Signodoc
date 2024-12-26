31a5011354f708aecf04ef07b4137d180efccdec
invedus <info@invedus.com>
2024-12-26 14:48:26 +0530

    first commit

---
+++ app/components/collaboration/index.tsx
@@ -0,0 +1,49 @@
+// import React from 'react'
+
+// type Props ={}
+// interface collabcontent {
+//     id: string,
+//     title:string,
+   
+// }
+// const collabcontent : collabcontent[] = [
+//     {
+//         id: "0",
+//         title: "Seamless Integration",
+        
+//       },
+//       {
+//         id: "1",
+//         title: "Smart Automation",
+//       },
+//       {
+//         id: "2",
+//         title: "Top-notch Security",
+//       },
+
+//     ]
+
+
+// const index = (props : Props) => {
+//   return (
+//     <section>
+//         <div className='container lg:flex text-black bg-white'>
+//             <div className='max-w-7xl'>
+//                 <h2 className='h2 mb-10 md:mb-10'>
+//                     Digital Signature App for seamless collaboration
+//                 </h2>
+//                 <ul className='mb-10 max-w-7xl md:mb-10'>
+//                     {collabcontent.map(item) => {
+
+//                     }
+                        
+//                     }
+//                 </ul>
+
+//             </div>
+//         </div>
+//     </section>
+//   )
+// }
+
+// export default index
